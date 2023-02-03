0f8de4eeaf88cecf84326ccdd5046c3b26e837cb
Fael Caporali <rafaelhon@gmail.com>
2023-02-03 18:05:10 -0300

    [feat - chore] basic test triggering function

---
+++ src/chore/trigger_test.py
@@ -0,0 +1,7 @@
+from templates.init_test import init_log
+import pytest
+
+
+def trigger_test(test_path: str):
+    init_log(test_path)
+    pytest.main([test_path, "--cache-clear", "-vv", "-n", "auto"])
